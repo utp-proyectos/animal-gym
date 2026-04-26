26387ddf0a92baca2ab28950ad73c7d2875d4d48
Mishell <indexclever@gmail.com>
2026-04-25 20:52:54 -0500

    fix: corregir iteracion de cards

---
+++ js/clases.js
@@ -0,0 +1,67 @@
+console.log('testing lcases')
+const clasesGym = [
+	{
+		titulo: 'Spinning',
+		descripcion: 'Clases de ciclismo indoor',
+		imagen: 'images/layout/clases/clase1.jpg',
+		icono: 'bi-bicycle',
+	},
+	{
+		titulo: 'Pilates',
+		descripcion: 'Clases de pilates con ejercicios de core',
+		imagen: 'images/layout/clases/clase2.jpg',
+		icono: 'bi-person-arms-up',
+	},
+	{
+		titulo: 'Boxeo Fitness',
+		descripcion: 'Entrenamiento de boxeo y cardio',
+		imagen: 'images/layout/clases/clase3.jpg',
+		icono: 'bi-hand-thumbs-up-fill',
+	},
+	{
+		titulo: 'Yoga Matutino',
+		descripcion: 'Clase de yoga para principiantes.',
+		imagen: 'images/layout/clases/clase5.jpg',
+		icono: 'bi-peace',
+	},
+	{
+		titulo: 'Crossfit',
+		descripcion: 'Entrenamiento funcional',
+		imagen: 'images/layout/clases/clase4.jpg',
+		icono: 'bi-bar-chart-line',
+	},
+	{
+		titulo: 'Zumba',
+		descripcion: 'Clase de baile fitness.',
+		imagen: 'images/layout/clases/clases6.jpg',
+		icono: 'bi-music-note-beamed',
+	},
+]
+
+const container = document.getElementById('row-container')
+
+container.innerHTML = ''
+
+clasesGym.forEach((clase) => {
+	const cardHTML = `
+            <div class="col-lg-4 col-md-6 col-12 mb-4">
+                <div class="card class-card border-0 shadow-lg overflow-hidden position-relative">
+                    <img src="${clase.imagen}" class="class-img" alt="${clase.titulo}">
+
+                    <div class="class-footer">
+                        <h5 class="text-white m-auto">${clase.titulo}</h5>
+                        <div class="class-icon">
+                            <i class="bi ${clase.icono} text-white display-6"></i>
+                        </div>
+                    </div>
+
+                    <div class="class-overlay">
+                        <i class="bi ${clase.icono} text-white display-6"></i>
+                        <h4 class="text-white fw-bold mb-2">${clase.titulo}</h4>
+                        <p class="text-white">${clase.descripcion}</p>
+                    </div>
+                </div>
+            </div>
+        `
+	container.innerHTML += cardHTML
+})
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import './membresia.js'
+import './clases.js'
 import { initNavbarScroll } from './components/navbar.js'
 
 document.addEventListener('DOMContentLoaded', () => {
